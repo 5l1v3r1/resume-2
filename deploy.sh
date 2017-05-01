e3e4097f86674f1b650120e79a7029c7f7bd8160
Steven Harris <stevenharrisdev@gmail.com>
2017-05-01 09:01:45 -0500

    removing pull
    might overwrite changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rimraf dist && webpack --config config/webpack.prod.js --progress --profile --bail
 docker build -t resume .
-docker pull steharris/resume:latest
 docker tag resume steharris/resume:latest
 docker push steharris/resume:latest
