17710a289821b6517abe3b65d1b79980fd1538d6
Steven Harris <SteHarris@jhacorp.com>
2017-08-24 16:43:17 -0500

    linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { Resume } from "./models/resume";
+import { Component, OnInit } from '@angular/core';
+import { Resume } from './models/resume';
 import {
   AngularFireDatabase,
   FirebaseListObservable
-} from "angularfire2/database";
+} from 'angularfire2/database';
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   public resume:FirebaseListObservable<any[]>;
   constructor(public db: AngularFireDatabase) {
-    this.resume = this.db.list("/resume");
+    this.resume = this.db.list('/resume');
   }
 
   ngOnInit() {
-    
+
   }
 }
