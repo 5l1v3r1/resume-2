358aae4239e3baed3e89747e631ade4aa7bb83c1
steven-harris <stevenvfx@hotmail.com>
2017-08-29 22:26:22 -0500

    updated docker config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM nginx
 COPY dist /usr/share/nginx/html
-COPY assets /usr/share/nginx/html/assets/
 EXPOSE 80
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-
-rimraf dist
 ng build –prod –aot
 docker build -t resume .
 docker tag resume steharris/resume:latest
 docker push steharris/resume:latest
+rimraf dist
