4dbbdfa6588a8e9a40464c816a898524a2a68250
Steven Harris <stevenharrisdev@gmail.com>
2017-09-25 20:56:52 -0500

    adding enivonment back

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAxyr7eHJxfWCiU4n2bXJB86hipyHow0q8',
+        authDomain: 'steven-harris-resume.firebaseapp.com',
+        databaseURL: 'https://steven-harris-resume.firebaseio.com',
+        projectId: 'steven-harris-resume',
+        storageBucket: 'steven-harris-resume.appspot.com',
+        messagingSenderId: '543213305210'
+    }
+};
